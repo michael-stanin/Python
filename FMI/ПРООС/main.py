79e24dfd7b3bba5814dfef27b25ae94724f222a5
michael-stanin <michael.stanin@gmail.com>
2015-04-09 22:19:08 +0300

    Separate the implementation.
    Separate the application class from the initializing.

--- FMI/ПРООС/main.py
+++ FMI/ПРООС/main.py
@@ -1,31 +1,21 @@
-import sys
-import tkinter as tk
+from application import Application
 
-
-class Application(tk.Tk):
+class Initializer:
 
     def __init__(self, *args, **kwargs):
-        tk.Tk.__init__(self, *args, **kwargs)
-        # Set window title
-        self.m_title = tk.Tk.title(self, string="Learn by practice")
-        # Set window size
-        #self.m_geometry = tk.Tk.geometry(self, newGeometry="800x600+250+100")
-        # Set the window icon
-        #self.m_iconbitmap = tk.Tk.iconbitmap(self, bitmap="")
-        self.m_user_name_label = tk.Label(text="username: ",
-                                         fg="blue").grid(row=0, column=0)
-        self.m_user_pass_label = tk.Label(text="password: ",
-                                         fg="blue").grid(row=1, column=0)        
+        pass
+
+    def start_up(self):
+        # Create a new window for the application
+        self.app = Application()
+
+        # Draw the window and start the application
+        self.app.mainloop()
 
 
-def Initialize():
-    # Create a new window for the application
-    app = Application()
 
-    # Draw the window and start the application
-    app.mainloop()
-    
 
 if __name__ == "__main__":
-    Initialize()
-    
+    init = Initializer()
+    init.start_up()
+        
