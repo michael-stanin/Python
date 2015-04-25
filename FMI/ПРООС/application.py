a79912afc3dae1e19072ad36aa4baca6fd795a0d
michael-stanin <michael.stanin@gmail.com>
2015-04-25 22:49:42 +0300

    Fix imports

--- FMI/ПРООС/application.py
+++ FMI/ПРООС/application.py
@@ -1,9 +1,16 @@
 import sys
 import tkinter as tk
 
+from filereader import FileReader 
+from filewriter import FileWriter
+
 
 class Application(tk.Tk):
 
+
+    users_storage = "users.txt"
+
+    
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         # Set window title
@@ -26,4 +33,29 @@ class Application(tk.Tk):
         self.background_label = tk.Label(self,image=self.background_image)
         self.background_label.pack()
 
+
+    def __users(self):
+        return [user.split() for user in FileReader.read(self.users_storage)]
+
+    def __correct_credentials(self, *args):
+        if any(args[0] == user[0] and args[1] == user[1]
+                            for user in self.__users()):
+            return True
+        return False
     
+    # Registering the users using (username, password, gender, privileges)
+    def register(self, *args):
+        if len(args) == 4 and not(any(args[0] == user[0]
+                            for user in self.__users())):
+            FileWriter.write(self.users_storage, *args)
+            print("{} registered!".format(args[0]))
+        else:
+            print("User name already exists. Try another user name.")
+
+    # Login using (username, password)
+    def login(self, *args):
+        if len(args) < 2:
+            print("Missing fields. Please enter both user name and password")
+        elif self.__correct_credentials(*args):
+            # TODO: Implement the logic after user login
+            print("{} successfully logged in!".format(args[0]))
