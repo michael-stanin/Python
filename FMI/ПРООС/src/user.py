96859468205eeec6bb74bad3bc31b478d2f2fa90
michael-stanin <michael.stanin@gmail.com>
2015-04-26 01:16:10 +0300

    New properties to user.

--- FMI/ПРООС/src/user.py
+++ FMI/ПРООС/src/user.py
@@ -11,7 +11,10 @@ class User:
 
     @first_name.setter
     def first_name(self, value):
-        self.first_name_ = value
+        if value != "":
+            self.first_name_ = value
+        else:
+            self.first_name_ = None
 
     @property
     def last_name(self):
@@ -19,4 +22,18 @@ class User:
 
     @last_name.setter
     def last_name(self, value):
-        self.last_name_ = value
+        if value != "":
+            self.last_name_ = value
+        else:
+            self.last_name_ = None
+
+    @property
+    def rights(self):
+        return self.rights_
+
+    @rights.setter
+    def rights(self, value):
+        if value in ("administrator", "student"):
+            self.rights_ = value
+        else:
+            self.rights_ = None
