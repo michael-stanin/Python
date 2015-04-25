e9385775651e40bd814995535b35c5f6a053d919
michael-stanin <michael.stanin@gmail.com>
2015-04-26 01:15:26 +0300

    Additional test....
    Adding the Student to test

--- FMI/ПРООС/tests/tests.py
+++ FMI/ПРООС/tests/tests.py
@@ -6,21 +6,59 @@ sys.path.insert(0, os.path.abspath(os.path.join(testdir, srcdir)))
 import unittest
 
 from user import User
+from student import Student
 
 class TestUser(unittest.TestCase):
 
-    def test_first_name(self):
-        name = "FirstName"
+    def test_valid_first_name(self):
+        first_name = "FirstName"
         user = User()
-        user.first_name = name
-        self.assertEqual(name, user.first_name)
+        user.first_name = first_name
+        self.assertEqual(first_name, user.first_name)
 
-    def test_last_name(self):
-        name = "LastName"
+    def test_invalid_first_name(self):
+        first_name = ""
         user = User()
-        user.last_name = name
-        self.assertEqual(name, user.last_name)
+        user.first_name = first_name
+        self.assertIsNone(user.first_name)
 
+    def test_valid_last_name(self):
+        last_name = "LastName"
+        user = User()
+        user.last_name = last_name
+        self.assertEqual(last_name, user.last_name)
+        
+    def test_valid_last_name(self):
+        last_name = ""
+        user = User()
+        user.last_name = last_name
+        self.assertIsNone(user.last_name)
 
+    def test_valid_rights(self):
+        rights = "administrator"
+        user = User()
+        user.rights = rights
+        self.assertEqual(rights, user.rights)
+
+    def test_invalid_rights(self):
+        rights = "GOD"
+        user = User()
+        user.rights = rights
+        self.assertIsNone(user.rights)
+
+class TestStudent(unittest.TestCase):
+    
+    def test_valid_gender(self):
+        gender = "male"
+        student = Student()
+        student.gender = gender
+        self.assertEqual(gender, student.gender)
+
+    def test_invalid_gender(self):
+        gender = "shemale"
+        student = Student()
+        student.gender = gender
+        self.assertIsNone(student.gender)
+        
 if __name__ == '__main__':
     unittest.main()
