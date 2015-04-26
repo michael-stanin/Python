a92e697f6bf7bf5f968f179818235a0cde3b72a9
michael-stanin <michael.stanin@gmail.com>
2015-04-26 04:47:41 +0300

    Add the tasks to the student.

--- FMI/ПРООС/src/student.py
+++ FMI/ПРООС/src/student.py
@@ -1,11 +1,13 @@
 from user import User
-
+from task import Task
 
 class Student(User):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
+        self.tasks_ = []
+        self.gender_ = ""
+        
     @property
     def gender(self):
         return self.gender_
@@ -15,4 +17,27 @@ class Student(User):
         if value in ("male", "female"):
             self.gender_ = value
         else:
-            self.gender_ = None
+            raise ValueError(
+                "{} is not possible value for gender!".format(value))
+    @property
+    def tasks(self):
+        return self.tasks_
+
+    @tasks.setter
+    def tasks(self, value):
+        if isinstance(value, list) and value:
+            self.tasks_ = value
+        else:
+            raise ValueError("Tasks for student are empty!")
+        
+    def add_task(self, task):
+        if isinstance(task, Task):
+            self.tasks_.append(task)
+        else:
+            raise ValueError("{} is not a Task".format(task))
+
+    def remove_task(self, task):
+        if task in self.tasks_:
+            self.tasks_.remove(task)
+        else:
+            raise KeyError("{} is not in student's tasks!".format(task))
