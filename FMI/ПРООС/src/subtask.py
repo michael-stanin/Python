054d7fb150c53cd1786d7b741922ada62c1ad4ed
michael-stanin <michael.stanin@gmail.com>
2015-04-26 04:46:06 +0300

    New classes in model.

---
+++ FMI/ПРООС/src/subtask.py
@@ -0,0 +1,42 @@
+
+
+class Subtask:
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    @property
+    def title(self):
+        return self.title_
+
+    @title.setter
+    def title(self, value):
+        if value != "":
+            self.title_ = value
+        else :
+            raise ValueError("Title for Subtask is empty!")
+
+    @property
+    def content(self):
+        return self.content_
+
+    @content.setter
+    def content(self, value):
+        if value != "":
+            self.content_ = value
+        else:
+            raise ValueError("Content for Subtask is empty!")
+
+    @property
+    def state(self):
+        return self.state_
+
+    @state.setter
+    def state(self, value):
+        # None will be used when the student hasn't started a subtask yet
+        # Started will be used when the student is working on the subtask
+        # Finished will be used when the student is done with the subtask
+        if value in ("None", "Started", "Finished"):
+            self.state_ = value
+        else:
+            raise ValueError("{} is not a correct state!")
---
+++ FMI/ПРООС/src/task.py
@@ -0,0 +1,52 @@
+from subtask import Subtask
+
+
+class Task:
+
+    def __init__(self, *args, **kwargs):
+        self.subtasks_ = []
+
+    @property
+    def title(self):
+        return self.title_
+
+    @title.setter
+    def title(self, value):
+        if value != "":
+            self.title_ = value
+        else :
+            raise ValueError("Title for Task is empty!")
+
+    @property
+    def content(self):
+        return self.content_
+
+    @content.setter
+    def content(self, value):
+        if value != "":
+            self.content_ = value
+        else:
+            raise ValueError("Content for Task is empty!")
+
+    @property
+    def subtasks(self):
+        return self.subtasks_
+
+    @subtasks.setter
+    def subtasks(self, value):
+        if isinstance(value, list) and value:
+            self.subtasks_ = value
+        else:
+            raise ValueError("Subtasks for Task are empty!")
+
+    def add_subtask(self, subtask):
+        if isinstance(subtask, Subtask):
+            self.subtasks_.append(subtask)
+        else:
+            raise ValueError("{} is not a Subtask".format(subtask))
+
+    def remove_subtask(self, subtask):
+        if subtask in self.subtasks_:
+            self.subtasks_.remove(subtask)
+        else:
+            raise KeyError("{} is not in student's tasks!".format(subtask))
