6086bf21b95669bcb260105792dfec0d9baedf2a
michael-stanin <michael.stanin@gmail.com>
2015-06-02 11:27:57 +0300

    Parser for the units info.

---
+++ FMI/ПРООС/src/unitparser.py
@@ -0,0 +1,19 @@
+from filereader import FileReader
+
+
+UNITS = "units.txt"
+
+
+class UnitParser:
+
+    def __init__(self):
+        self.units_ = FileReader.read_units(UNITS)
+
+    def parse(self):
+        unit_names = [name for name in self.units_[::3] if name]
+        unit_contents = [content for content in self.units_[1::3] if content]
+
+        self.units_ = dict(zip(unit_names, unit_contents))
+
+    def units(self):
+        return self.units_
